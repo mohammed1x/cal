a4de60721b1d3b684e3c6686dcb6e9915946f813
mohammed1x <141856219+mohammed1x@users.noreply.github.com>
2024-02-02 10:28:48 +0530

    Create cal.sh

---
+++ cal.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while true; do
+    echo "---------------------------------------------"
+    echo "                   M0h4mm3d                   "
+    echo "---------------------------------------------"
+    read -p "Enter the scale (default=30, q to quit): " scale
+
+    # Check if the user wants to quit
+    if [ "$scale" == "q" ]; then
+        echo "Exiting..."
+        break
+    fi
+
+    # Set default scale if user input is empty
+    scale="${scale:-30}"
+
+    read -p "Enter the expression: " expression
+
+    # Perform the calculation and remove line breakers and slashes
+    result=$(echo "scale=${scale};${expression}" | bc -l -w | tr -d '\n\\')
+
+    # Display the result
+    echo "Result: ${result}"
+
+    # Clear the result variable for the next iteration
+    unset result
+done
